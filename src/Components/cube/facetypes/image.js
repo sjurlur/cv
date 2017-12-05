16cd97c8fc57386e70dbd1b443a328769f8792ef
Sjur Seibt <sjurlur@gmail.com>
2017-12-05 20:37:00 +0100

    Add images

--- src/Components/cube/facetypes/image.js
+++ src/Components/cube/facetypes/image.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Image = styled.img `
-max-width: 100%;
+const Image = styled.div `
+width: 100%;
+height: 100%;
+opacity: 0.7;
+background: url('${props => props.url}');
+background-size: cover;
 `
 
 export default props => {
-    return(<Image />);
+    return(<Image url={props.data}/>);
 }
